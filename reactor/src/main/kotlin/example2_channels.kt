2278036fcde3dd3ae34a1fef6419820b5dd498bf
Alexis Manin <alexis.manin@gmail.com>
2022-05-22 09:30:41 +0200

    Refactor Reactor example 2, try to describe implementation choice.

--- reactor/src/main/kotlin/example2_channels.kt
+++ reactor/src/main/kotlin/example2_channels.kt
@@ -1,26 +1,31 @@
 package fr.amanin.concurrency.examples.reactor
 
+import reactor.core.publisher.Flux
+import reactor.core.publisher.FluxSink
 import reactor.core.publisher.Mono
 import reactor.core.publisher.Sinks
-import reactor.core.publisher.Sinks.EmitResult.OK
 import reactor.core.scheduler.Schedulers
 import java.time.Duration
 
-// TODO: this does not match example 2. This is 'more' example 3, I think.
-
 /**
- * Reactor equivalent of a buffered channel is a "sink".
- * In reactive stream terms, it is a "hot" flux (in short: non-repeatable stream, triggered by producer instead of consumer).
+ * Note: I cannot find an equivalent to Go non-buffered channels. Reactive-streams work a little differently.
+ * They use user requests to trigger a set of production action.
+ * The closest APIs for channels are [FluxSink] and [reactor.core.publisher.Sinks.Many].
+ * However, they're both behave more like buffered channels than unbuffered ones.
+ *
+ * In this example, we'll use [FluxSink]. It differs from Go channel example, because here, the sum processing won't
+ * start until a consumer subscribe to the link data stream. Once a user connect to the channel, both sums will be
+ * computed and submitted without further suspension/waiting.
+ *
+ * The example could have used [Sinks.Many] (see previous file version in Git repository). However, it makes it more
+ * complex, because "hot streams" (ie. non-repeatable flows triggered upstream by data producer instead of consumer)
+ * require a lot of additional control code to ensure good wealth and back-pressure control.
  */
-fun sum(values: IntArray, from: Int, to: Int, channel: Sinks.Many<Int>) {
+fun sum(values: IntArray, from: Int, to: Int, channel: FluxSink<Int>) {
     var sum = 0
     for (i in from until to) sum += values[i]
-    val result = channel.tryEmitNext(sum)
-    when (result) {
-        // Here, we could manage different emission failures. But there's no need in this simple example
-        OK   -> log("$sum emitted")
-        else -> throw IllegalStateException("Cannot push sum to the channel. Emission failure: $result")
-    }
+    channel.next(sum)
+    log("$sum emitted")
 }
 
 /*
@@ -37,31 +42,19 @@ fun go(title: String, action: () -> Unit) = Mono.fromRunnable<Void>(action)
 
 fun main() {
     val values = intArrayOf(7, 2, 8, -9, 4, 0)
-    /*
-     * There's a lot of Hot streams
-     */
-    val channel = Sinks.many().multicast().onBackpressureBuffer<Int>()
+
     val half = values.size / 2
-    go("first half") { sum(values, 0, half, channel) }
-    go("second half") { sum(values, half, values.size, channel) }
+
+    val channel = Flux.create<Int> { sink ->
+        go("first half") { sum(values, 0, half, sink) }
+        go("second half") { sum(values, half, values.size, sink) }
+    }
 
     log("Tasks submitted !")
 
     val (x, y) = Mono.delay(Duration.ofMillis(100))
-        .then(
-            channel.asFlux()
-                .take(2)
-                .collectList()
-        )
+        .then(channel.take(2).collectList())
         .block() ?: throw IllegalStateException("Cannot take value from channel")
 
     println("$x  + $y = ${x + y}")
-
-    val complete = channel.tryEmitComplete()
-    log {
-        when (complete) {
-            OK -> "Channel closed"
-            else -> "Error while closing channel: $complete"
-        }
-    }
 }
