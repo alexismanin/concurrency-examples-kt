977bd6861de5d662bb88ee2a9ccc048c3e9b2bd4
Alexis Manin <alexis.manin@gmail.com>
2022-05-25 06:37:37 +0200

    [Reactor] add Example 4: Range and close

---
+++ reactor/src/main/kotlin/example4_rangeclose.kt
@@ -0,0 +1,34 @@
+package fr.amanin.concurrency.examples.reactor
+
+import reactor.core.publisher.SignalType
+import reactor.core.publisher.Sinks
+import reactor.core.publisher.Sinks.EmitResult
+import java.util.concurrent.ArrayBlockingQueue
+
+private fun failFast(s: SignalType, r: EmitResult) : Boolean = throw IllegalStateException("Cannot emt signal $s. Reason: $r")
+
+fun fibonacci(n: Int, output: Sinks.Many<Int>) {
+
+    var next = 0 to 1
+    for (i in 1..n) {
+        val (x, y) = next
+        output.emitNext(x, ::failFast)
+        next = y to x + y
+    }
+
+    output.emitComplete(::failFast)
+}
+
+fun main() {
+    val capacity = 10
+
+    val transport = Sinks.many().unicast().onBackpressureBuffer<Int>(ArrayBlockingQueue(10))
+
+    go("Compute Fibonacci") { fibonacci(capacity, transport) }
+
+    // TODO: try to reproduce advanced consumption behavior like in coroutine example.
+    // Consuming the sink using two different subscribers does not produce expected result.
+    transport.asFlux()
+        .doOnNext { println(it) }
+        .blockLast()
+}
